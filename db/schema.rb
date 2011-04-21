d9ac246a9333eb25b9aac638f65d55a71f64f841
Simon Fenton <swfenton@gmail.com>
2011-04-21 12:35:04 +1000

    Add carts. Modify the application controller to put the cart id in the session.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110409125431) do
+ActiveRecord::Schema.define(:version => 20110421021323) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
