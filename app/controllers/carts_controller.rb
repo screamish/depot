dcbe9373a40abfe95fce294a265180eb63579b82
Simon Fenton <swfenton@gmail.com>
2011-04-22 13:52:06 +1000

    Made 'Empty cart' an ajax action, and now the cart vanishes using blind_up when being emptied.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -83,6 +83,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(store_url) }
+      format.js { @cart = current_cart }
       format.xml  { head :ok }
     end
   end
